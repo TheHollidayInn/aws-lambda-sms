16c0315d5e879b1fd9489259a5087888fbbbbc03
Keith Holliday <krh121791@yahoo.com>
2018-09-11 10:41:04 -0500

    initial commit

---
+++ MyFirstProject/lambda.js
@@ -0,0 +1,35 @@
+let AWS = require('aws-sdk');
+const sns = new AWS.SNS();
+exports.handler = function (event, context, callback) {
+
+	let receiver = event['receiver'];
+	let sender = event['sender'];
+	let message = event['message'];
+
+	console.log("Sending message", message, "to receiver", receiver);
+
+	sns.publish({
+		Message: message,
+		MessageAttributes: {
+			'AWS.SNS.SMS.SMSType': {
+				DataType: 'String',
+				StringValue: 'Promotional'
+			},
+			'AWS.SNS.SMS.SenderID': {
+				DataType: 'String',
+				StringValue: sender
+			},
+		},
+		PhoneNumber: receiver
+	}).promise()
+		.then(data => {
+			console.log("Sent message to", receiver);
+			callback(null, data);
+		})
+		.catch(err => {
+			console.log("Sending failed", err);
+			callback(err);
+		});
+
+	
+}
